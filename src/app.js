f1e8cf0450b2564d3383e8691b878386cf2193e9
jalbu002 <jalbu002@fiu.edu>
2023-03-15 22:22:27 +0000

    initial commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,33 @@
 /* eslint-disable */
-import "bootstrap";
+
 import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+let generateRandomNumber = () => {
+  let numbers = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
+  let indexNumbers = Math.floor(Math.random() * numbers.length);
+  return numbers[indexNumbers];
+};
+
+let generateRandomSuit = () => {
+  let suit = ["♦", "♥", "♠", "♣"];
+  let indexSuit = Math.floor(Math.random() * suit.length);
+  if (suit[indexSuit] == "♥" || suit[indexSuit] == "♦") {
+    let el = document.querySelector(".top-suit");
+    el.classList.add("red");
+    let el2 = document.querySelector(".bottom-suit");
+    el2.classList.add("red");
+  }
+  return suit[indexSuit];
+};
+
+window.onload = () => {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector(".number").innerHTML = generateRandomNumber();
+
+  let v = generateRandomSuit();
+  document.querySelector(".bottom-suit").innerHTML = v;
+  document.querySelector(".top-suit").innerHTML = v;
 };
